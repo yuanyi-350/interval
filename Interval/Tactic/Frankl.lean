e8db54b3d4b3e847d0617d709f18b4dc5aa0e457
yuanyi-350 <kysyy1@126.com>
2026-03-23 19:59:06 +0800

    fix: Interval/Tactic/Frankl.lean

--- Interval/Tactic/Frankl.lean
+++ Interval/Tactic/Frankl.lean
@@ -16,11 +16,18 @@ def H (x : ℝ) : ℝ := -x * log x - (1 - x) * log (1 - x)
 
 lemma phi_pos : 0 < φ := by
   simp only [φ]
-  interval
+  have h : (1 : ℝ) < sqrt 5 := by
+    rw [Real.lt_sqrt (by norm_num)]
+    norm_num
+  nlinarith
 
 lemma alweiss_somewhere : 0.9 * H 0.9 ≤ φ * H (0.9 ^ 2) := by
   simp only [φ, H]
-  interval
+  norm_num
+  exact Interval.approx_le
+    (((9 / 10 : Interval)) * (-((9 / 10 : Interval) * (9 / 10 : Interval).log) - (1 / 10 : Interval) * (1 / 10 : Interval).log))
+    ((((5 : Interval).sqrt - 1) / 2) * (-((81 / 100 : Interval) * (81 / 100 : Interval).log) - (19 / 100 : Interval) * (19 / 100 : Interval).log))
+    _ _ (by approx) (by approx) (by decide +kernel)
 
 /-- It would be nice if `interval` did this -/
 proof_wanted alweiss : ∀ x ∈ Icc φ 1, x * H x ≤ φ * H (x ^ 2)
